a9451f21421163987e6532afd62b88399d69a394
nikiryota0731 <rniki025@gmail.com>
2019-08-11 09:14:32 +0900

    LCDの表示を変更

--- main_2/display.cpp
+++ main_2/display.cpp
@@ -13,18 +13,22 @@ void setDisplay(uint8_t dataPageNum, uint8_t dataNum, int SendData) {
   for (int i = 0; i < 4; i++) {
     i2cLCD.setData(i * 2, (sendData[pageNum][i] >> 8) & 0x00ff);
     i2cLCD.setData(i * 2 + 1, sendData[pageNum][i] & 0x00ff);
-  }
+  } 
   i2cLCD.setData(8, pageNum);
 }
 void display() {
   setDisplay(0, 0, position.getNowX());
   setDisplay(0, 1, position.getNowY());
   setDisplay(0, 2, gyro.getData() * 10);
-
   
-  setDisplay(2, 0, i2cOmni.getSendData(0));
-  setDisplay(2, 1, i2cOmni.getSendData(1));
-  setDisplay(2, 2, i2cOmni.getSendData(2));
+  setDisplay(1, 0, position.getNowX());
+  setDisplay(1, 1, position.getNowY());
+  setDisplay(1, 2, omniDriver.getMoveX());
+  setDisplay(1, 3, omniDriver.getMoveY());
+
+  setDisplay(2, 0, omniDriver.getSendData(0));
+  setDisplay(2, 1, omniDriver.getSendData(1));
+  setDisplay(2, 2, omniDriver.getSendData(2));
 
   setDisplay(3, 0, i2cMotorBoard.getSendData(1));
   setDisplay(3, 1, i2cMotorBoard.getSendData(2));
