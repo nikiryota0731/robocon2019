c412755b1f003c6f65c29d3ee4fb48854c9783f0
nikiryota0731 <rniki025@gmail.com>
2019-08-11 09:12:30 +0900

    omni_driver.hの追加
    class以前のものをコメントアウト

--- main_2/omniAuto.cpp
+++ main_2/omniAuto.cpp
@@ -1,78 +1,90 @@
-#include "sub.h"
-
-/*  ジャイロ */
-extern int gyroOffset;
-
-const int ROBOTWIDTH_X = 700;
-const int ROBOTWIDTH_Y = 700;
-
-
-int locationState = 0;
-bool isPause = false;
-bool isTargetLocation = false;
-
-bool getIsTargetLocation(int nowX, int nowY, int moveX, int moveY) {
-  return getDistance(nowX, nowY, moveX, moveY) < 20;
-}
-
-void omniAuto(int nowX, int nowY) {
-  static int locations[][3] = {
-    {600, 1800, 40},
-    {600, 1800 + 2000, 40},
-    {600 + 1800, 1800 + 2000, 40},
-    {600 + 1800, 1800 + 2000 + 800, 20},
-    {600 + 1800 + 1500, 1800 + 2000 + 800, 20},
-    {600 + 1800 + 1500, 1800 + 2000 - 800, 20},
-    {600 + 1800, 1800 + 2000 - 800, 20},
-    {600 + 1800, 1800 + 2000, 20},
-    {600, 1800 + 2000, 40},
-    {600, 1800, 40}
-  };
-  int moveX = locations[locationState][0];
-  int moveY = locations[locationState][1];
-
-  omniXPid.update(moveX - nowX, 0, locations[locationState][2]);
-  omniYPid.update(moveY - nowY, 0, locations[locationState][2]);
-  i2cOmni.setData(0, map(-omniXPid.getPower(), -255, 255, 0, 255));
-  i2cOmni.setData(1, map(-omniYPid.getPower(), -255, 255, 0, 255));
-
-  gyroPid.update(gyro.getData(), 0);
-  i2cOmni.setData(2, map(gyroPid.getPower(), 255, -255, 0, 255));
-
-  if (locationState == 0) {
-    isStart = false;
-    if (limitSW[0].getClick(FALLING) || DS3.getCircleClick()) {
-      isStart = true;
-    }
-  }
-  isTargetLocation = getIsTargetLocation(nowX, nowY, moveX, moveY);
-
-  if (isStart) {
-    if (!isPause) {
-      if (isTargetLocation || DS3.getCircleClick()) {
-        //      if (DS3.getCircleClick()) {
-        locationState++;
-        isTargetLocation = false;
-        if (locationState >= 9)
-          locationState = 0;
-      }
-      if (DS3.getCrossClick()) {
-        locationState--;
-      }
-    }
-  }
-
-
-
-  setDisplay(1, 0, nowX);
-  setDisplay(1, 1, nowY);
-  setDisplay(1, 2, moveX);
-  setDisplay(1, 3, moveY);
-
-  setDisplay(2, 3, isTargetLocation * 88);
-
-  //Serial.print(isStart);
-  //Serial.print("\t");
-  //Serial.println(locationState);
-
-}
+//#include "sub.h"
+//
+///*  ジャイロ */
+//extern int gyroOffset;
+//
+//const int ROBOTWIDTH_X = 700;
+//const int ROBOTWIDTH_Y = 700;
+//
+//
+//int locationState = 0;
+//bool isPause = false;
+//bool isTargetLocation = false;
+//
+//bool getIsTargetLocation(int nowX, int nowY, int moveX, int moveY) {
+//  return getDistance(nowX, nowY, moveX, moveY) < 20;
+//}
+//
+//void omniAuto(int nowX, int nowY) {
+//  static int locations[10][3] = {
+//    {600, 1800, 40},
+//    {600, 1800 + 2000, 40},
+//    {600 + 1800, 1800 + 2000, 40},
+//    {600 + 1800, 1800 + 2000 + 800, 20},
+//    {600 + 1800 + 1500, 1800 + 2000 + 800, 20},
+//    {600 + 1800 + 1500, 1800 + 2000 - 800, 20},
+//    {600 + 1800, 1800 + 2000 - 800, 20},
+//    {600 + 1800, 1800 + 2000, 20},
+//    {600, 1800 + 2000, 40},
+//    {600, 1800, 40}
+//  };
+//
+//  int moveX = locations[locationState][0];
+//  int moveY = locations[locationState][1];
+//
+//  omniXPid.update(moveX - nowX, 0, locations[locationState][2]);
+//  omniYPid.update(moveY - nowY, 0, locations[locationState][2]);
+//  i2cOmni.setData(0, map(-omniXPid.getPower(), -255, 255, 0, 255)); 
+//  i2cOmni.setData(1, map(-omniYPid.getPower(), -255, 255, 0, 255));
+//
+//  gyroPid.update(gyro.getData(), 0);
+//  i2cOmni.setData(2, map(gyroPid.getPower(), 255, -255, 0, 255));
+//
+//  if (locationState == 0) {
+//    isStart = false;
+//    if (limitSW[0].getClick(FALLING) || DS3.getCircleClick()) {
+//      isStart = true;
+//    }
+//  }
+//  isTargetLocation = getIsTargetLocation(nowX, nowY, moveX, moveY);
+//
+//  if (isStart) {
+//    if (!isPause) {
+//      //      if (isTargetLocation || DS3.getCircleClick()) {
+//      if (DS3.getCircleClick()) {
+//        locationState++;
+//        isTargetLocation = false;
+//        if (locationState >= 10)
+//          locationState = 0;
+//      }
+//      if (DS3.getCrossClick()) {
+//        locationState--;
+//      }
+//    }
+//  }
+//
+//
+//
+//  setDisplay(1, 0, nowX);
+//  setDisplay(1, 1, nowY);
+//
+//  setDisplay(1, 2, moveX);
+//  setDisplay(1, 3, moveY);
+//
+//  setDisplay(2, 3, isTargetLocation * 88);
+//
+//  Serial.print(nowX);
+//  Serial.print("\t");
+//  Serial.print(nowY);
+//  Serial.print("\t");
+//  Serial.print(moveX);
+//  Serial.print("\t");
+//  Serial.print(moveY);
+//  Serial.print("\t");
+//  Serial.print(isTargetLocation);
+//  Serial.print("\t");
+//  Serial.print(locationState);
+//  Serial.print("\t");
+//
+//
+//}
--- main_2/omniManual.cpp
+++ main_2/omniManual.cpp
@@ -1,37 +1,37 @@
-#include "sub.h"
-
-int omniTest() {
-  static int turnValue = 0;
-  if (DS3.getLeftClick())
-    turnValue += 5;
-  else if (DS3.getRightClick())
-    turnValue -= 5;
-  if (DS3.getStartClick())
-    turnValue = 0;
-  return turnValue;
-
-}
-
-void omniManual(int moveX, int moveY, int turn) {
-  moveX = map(moveX, 0, 255, 127 - 50, 127 + 50);
-  moveY = map(moveY, 255, 0, 127 - 50, 127 + 50);
-  if (abs(moveX - 127) < 5)
-    moveX = 127;
-  if (abs(moveY - 127) < 5)
-    moveY = 127;
-  i2cOmni.setData(0, moveX);
-  i2cOmni.setData(1, moveY);
-  if (DS3RightHatX.getClick(FALLING)) {
-    gyro.setOffset();
-  }
-
-  if (!DS3RightHatX.getData()) {
-    gyroPid.update(gyro.getData(), 0);
-    i2cOmni.setData(2, map(gyroPid.getPower(), 255, -255, 0, 255));
-  } else
-    i2cOmni.setData(2, map(DS3.getRightHatX(), 255, 0, 127 - 30, 127 + 30) );
-  //  Serial.write((angleDeg >> 8) & 0x00ff);
-  //  Serial.write(angleDeg & 0x00ff);
-  //  Serial.write(!DS3RightHatX.getData());
-
-}
+//#include "sub.h"
+//
+//int omniTest() {
+//  static int turnValue = 0;
+//  if (DS3.getLeftClick())
+//    turnValue += 5;
+//  else if (DS3.getRightClick())
+//    turnValue -= 5;
+//  if (DS3.getStartClick())
+//    turnValue = 0;
+//  return turnValue;
+//
+//}
+//
+//void omniManual(int moveX, int moveY, int turn) {
+//  moveX = map(moveX, 0, 255, 127 - 50, 127 + 50);
+//  moveY = map(moveY, 255, 0, 127 - 50, 127 + 50);
+//  if (abs(moveX - 127) < 5)
+//    moveX = 127;
+//  if (abs(moveY - 127) < 5)
+//    moveY = 127;
+//  i2cOmni.setData(0, moveX);
+//  i2cOmni.setData(1, moveY);
+//  if (DS3RightHatX.getClick(FALLING)) {
+//    gyro.setOffset();
+//  }
+//
+//  if (!DS3RightHatX.getData()) {
+//    gyroPid.update(gyro.getData(), 0);
+//    i2cOmni.setData(2, map(gyroPid.getPower(), 255, -255, 0, 255));
+//  } else
+//    i2cOmni.setData(2, map(DS3.getRightHatX(), 255, 0, 127 - 30, 127 + 30) );
+//  //  Serial.write((angleDeg >> 8) & 0x00ff);
+//  //  Serial.write(angleDeg & 0x00ff);
+//  //  Serial.write(!DS3RightHatX.getData());
+//
+//}
